f33dd305ded6bd36d7a85ae66da7ccccb7cb1a19
artem <work.aab.25122001@gmail.com>
2024-11-25 09:59:44 +0000

    tweak run env

---
+++ backend/start_dev.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+export PORT=8080 
+export ADDR=0.0.0.0 
+export DATA_FOLDER=${ROOT}/data 
+export PASSWORD=dev 
+export STATIC_DIR=../static
+cargo run
--- run.sh
+++ run.sh
@@ -1,3 +1,4 @@
 ROOT="$(dirname "$(realpath "$0")")"
+
 cd $ROOT/frontend && pnpm run watch &
-cd $ROOT/backend && PORT=8080 ADDR=0.0.0.0 DATA_FOLDER=${ROOT}/data PASSWORD=dev STATIC_DIR=../static cargo watch -x run
+cd $ROOT/backend && cargo watch -s "$ROOT/backend/start_dev.sh"
